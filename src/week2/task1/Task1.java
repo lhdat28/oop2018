260b186b7d6bb97c070343e83fa16dbb8fb599d7
lhdat28 <43239085+lhdat28@users.noreply.github.com>
2018-09-27 10:54:54 -0500

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,36 @@
 package week2.task1;
 
-public class Task1 {
+import java.util.Scanner;
 
+public class Task1 {
     public static int gcd(int a, int b) {
+        while(a!=b)
+        {
+            if(a>b) a=a-b;
+            else b=b-a;
+        }
+        return a;
+
+
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
     }
 
     public static int fibonacci(int n) {
+        if (n==0 ) return 0;
+        else if (n==1) return 1;
+        else  {
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
     }
-}
+    public static void main(String[] args){
+        int a=2;
+        int b=8;
+        System.out.println("Uoc chung lon nhat la "+gcd(a,b));
+        for (int i=0; i<35; i++){
+            System.out.println( +fibonacci(i)+" ");
+        }
+
+
+
+    }
