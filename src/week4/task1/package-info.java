e6e171611ce5ac662b9dff7cdf31787c405d67b3
lhdat28 <43239085+lhdat28@users.noreply.github.com>
2018-10-11 22:37:18 -0500

    Update package-info.java

--- src/week4/task1/package-info.java
+++ src/week4/task1/package-info.java
@@ -7,3 +7,24 @@
  * @version 0.1
  */
 package week4.task1;
+
+public class CamSanh extends Cam {
+    private float cost;
+    CamSanh(){};
+    CamSanh(String name,float cost ) {
+        this.setName(name);
+        this.setType("Ngon Ngọt");
+        this.cost=cost;
+        this.setType("Cam");
+    }
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+    public float getCost(){
+        return cost;
+    }
+
+    public String toString(){
+        return this.getName()+" "+this.getTaste()+" gia tien la: "+cost+" 1kg ";
+    }
+}
